fd89a277105f17faace6d6932301b57ccafff15f
Qk-Z-T <tamim640823@gmail.com>
2026-04-14 08:28:09 +0600

    Add Tamim Hossain as a contributor

---
+++ contributor/data/tamim.js
@@ -0,0 +1,16 @@
+// contributor/data/tamim.js
+window.__CONTRIBUTORS__ = window.__CONTRIBUTORS__ || [];
+window.__CONTRIBUTORS__.push({
+    name: "Tamim Hossain",
+    title: "Web Developer & Science Enthusiast",
+    image: "../assets/images/tamim.png",
+    fallbackIcon: "fa-user-astronaut",
+    details: [
+        { icon: "fa-code", label: "Dev", value: "React, JS, HTML/CSS" },
+        { icon: "fa-brain", label: "Vibe", value: "Physics, Chess, Football" }
+    ],
+    social: {
+        github: "https://github.com/tamim",
+        linkedin: "https://linkedin.com/in/tamim"
+    }
+});
