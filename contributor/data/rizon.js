0d5c45e5c704671b72f0487f15b6f958957d2c24
Qk-Z-T <tamim640823@gmail.com>
2026-04-16 08:04:50 +0600

    Add contributor Raison Islam Rizom details

---
+++ contributor/data/rizon.js
@@ -0,0 +1,15 @@
+// contributor/data/tamim.js
+window.__CONTRIBUTORS__ = window.__CONTRIBUTORS__ || [];
+window.__CONTRIBUTORS__.push({
+    name: "Raisul Islam.Rizom",
+    title: "Adviser,motivator",
+    image: "../assets/images/Screenshot_20260416-080028.jpg",
+    fallbackIcon: "fa-user-astronaut",
+    details: [
+        { icon: "fa-school", label: "College", value: "Govt. Keshab Chandra College" },
+        { icon: "fa-brain", label: "Vibe", value: "Music,movies,cricket" }
+    ],
+    social: {
+        facebook: "https://www.facebook.com/https://www.facebook.com/share/17ehJVxS9i/"   // ← শুধু ফেসবুক
+    }
+});
